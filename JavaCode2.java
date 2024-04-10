e12e6ddbde43d72d5954b24185c883b8a87d8535
Anumit Jooloor <anumit@gmail.com>
2024-04-10 11:25:49 -0700

    Add files

--- JavaCode2.java
+++ JavaCode2.java
@@ -28,7 +28,28 @@ public class JavaCode2 {
         // Finding a Character in a String
         System.out.println(lord_of_the_rings_3.indexOf("Rings"));
 
-        
+        // String replace method
+        String IPaddress = "104.21.68.182"; // Official Massachusetts Travel Guide - visitma.com
+        String defanged_IPaddress = IPaddress.replace(".", "[.]");
+        System.out.println("Original = " + IPaddress + ", New = " + defanged_IPaddress);
+
+        // Find a character in a string 
+        String mithril = "Mithril";
+        char character_found = mithril.charAt(3); // 0 based indexing
+        System.out.println("String = " + mithril + ", Character at index 3 = " + character_found);
+
+        // Array of strings
+        String[] lord_of_the_rings_cities = {"Minas Tirith", "Gondor", "Edoras" };
+        for (int i =0; i < lord_of_the_rings_cities.length; i++) {
+            System.out.println(lord_of_the_rings_cities[i]);
+        }
+
+        // String equals 
+        String city1 = "Lindon";
+        System.out.println("Lindon string equals = " + city1.equals("Lindon"));
+
+        String lord_of_the_rings_best_quote = "One ring to rule them all, one ring to find them, one ring to bring them all and in the darkness bind them.";
+
         
 
 
